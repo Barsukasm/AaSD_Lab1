bac8fcd823f32b9d63c348d4d1e8f9503ffed1f0
Barsukasm <nb721@mail.ru>
2018-09-10 00:13:04 +0700

    Almost done. TODO: Change DeleteByNumber, add Test.

--- List.h
+++ List.h
@@ -1,30 +1,50 @@
+#include <iostream>
+
+#define INVALID_INDEX 0
+#define EMPTY_LIST 1
+#define ITERATOR_END 2
 
 template <typename T>
 class List{
-private:
-    int size;//размер списка
-    Node *head;//указатель на начало списка
 public:
     List();//конструктор по умолчанию
     List(List<T> &list);//конструктор копирования
     ~List();//деструктор
 
     //методы интерфейса
-    void AddItem(T t);//добавление нового элемента в начало списка
+    void AddItem(T t);//добавление нового элемента в список
     bool AddItemByNumber(T t, int pos);//добавление элемента в позицию с номером pos
     void CleanList();//очистка списка
     bool DeleteItem();//удаление из начала списка
     bool DeleteByNumber(int pos);//удалить элемент с номером pos
     bool DeleteByValue(T t);//удаление элемента по значению
-    int FindItem(T t);//опрос списка на наличие заданного значения
+    bool FindItem(T t);//опрос списка на наличие заданного значения
     T FindItemByNumber(int pos);//чтение элемента с индексом pos
     int GetPos(T t);//получение позиции элемента с заданным значением
     bool isEmpty();//проверка списка на пустоту
     int GetSize();//получить длинну списка
     bool ChangeValue(T t, int pos);//изменить значение с заданным номером в списке
 
+
+
+    friend class Iterator;
+
+    class Node{
+    public:
+        T *item;
+        Node *next;
+        Node(T t);//конструктор элемента
+        Node();//конструктор по умолчанию
+        ~Node();//деструктор
+    };
+
+    friend class Node;
+
     class Iterator{
         List<T> *list;//текущий список
+        Node *cur;//указатель на текущий элемент
+        bool status;//обошел список или нет
+
     public:
         Iterator(List<T> &t);
         Iterator &operator++(int);//переход к следующему элементу
@@ -33,23 +53,266 @@ public:
         bool inList();//проверка на нахождение внутри списка
     };
 
-    friend class Iterator;
-
-    class Node{
-        T Item;
-        Node *next;
-        Node(T t);//конструктор элемента
-        Node();//конструктор по умолчанию
-        ~Node();//деструктор
-    };
-
-    friend class Node;
+private:
+    int size;//размер списка
+    Node *head;//указатель на начало списка
+    Node *tail;//указатель на последний элемент списка
 };
 
+//------------Методы класса List---------------
 
-template <T>
-List::List() {
+//Конструктор по умолчанию
+template <typename T>
+List<T>::List() {
     head=NULL;
+    tail=NULL;
     size=0;
 }
 
+//Конструктор клонирования
+template <typename T>
+List<T>::List(List<T> &list) {
+    size = list.size;
+    if(list.GetSize == 0){
+        head->next=NULL;
+        tail->next=NULL;
+    } else{
+        Node *t = list.head;
+        for(int i=0;i<list.GetSize();i++){
+            AddItem(t->item);
+            t = t->next;
+        }
+    }
+}
+
+//Деструктор
+template <typename T>
+List<T>::~List() {
+    CleanList();
+}
+
+//включение нового элемента в конец списка
+template <typename T>
+void List<T>::AddItem(T t) {
+    if(size==0){
+        head =new Node(t);
+        tail=head;
+        tail->next=head;
+    } else{
+        Node *tmp=new Node(t);
+        tmp->next=head;
+        tail->next=tmp;
+        tail=tmp;
+    }
+    size++;
+}
+
+//включение нового значения в позицию с заданным номером
+template <typename T>
+bool List<T>::AddItemByNumber(T t, int pos) {
+    if(pos>=size||pos<0) return false;
+    else{
+        Node *added = new Node(t);
+
+        Node *tmp=head;
+        int i=0;
+        while (i!=pos){
+            tmp=tmp->next;
+            i++;
+        }
+        added->next=tmp->next;
+        tmp->next=added;
+        size++;
+        if(pos==size-1){
+            tail=added;
+        }
+        return true;
+    }
+}
+
+template <typename T>
+bool List<T>::DeleteItem() {
+    bool removed= false;
+    if(head==NULL) return removed;
+    else{
+        if(tail==head){
+            delete head;
+            head=NULL;
+            tail=NULL;
+            size--;
+            removed=true;
+        } else{
+            tail->next=head->next;
+            delete head;
+            head=tail->next;
+            size--;
+            removed=true;
+        }
+        return removed;
+    }
+}
+
+template <typename T>
+bool List<T>::DeleteByNumber(int pos) {//переделать
+    if(pos>=size||pos<0) return false;
+    Node *tmp=head;
+    Node *prev=tail;
+    int i=0;
+    while(i!=pos){
+        tmp=tmp->next;
+        prev=prev->next;
+        i++;
+    }
+    if(i==0){
+        bool res = DeleteItem();
+        return res;
+    }
+    prev->next=tmp->next;
+    delete tmp;
+    if(pos==size-1) tail = prev;
+    size--;
+    return true;
+
+}
+
+template <typename T>
+bool List<T>::DeleteByValue(T t) {
+    Node *tmp=head;
+    Node *prev=tail;
+
+    for(int i=0;i<size;i++){
+        if(*(tmp->item)==t){
+            prev->next=tmp->next;
+            delete tmp;
+            size--;
+            if(i==0) head=tail->next;
+            if(i==size-1) tail=prev;
+            return true;
+        }
+        tmp=tmp->next;
+        prev=prev->next;
+    }
+    return false;
+}
+
+template <typename T>
+void List<T>::CleanList() {
+    Node* tmp;
+    for(int i=0;i<size;i++){
+        tmp = head->next;
+        head->next=tmp->next;
+        delete tmp;
+    }
+    head=NULL;
+    tail=NULL;
+    size=0;
+}
+
+template <typename T>
+bool List<T>::FindItem(T t) {
+    Node *tmp=head;
+    for(int i=0;i<size;i++){
+        if(*(tmp->item)==t) return true;//возвращаем true, если было найдено соответствующее значение
+        tmp=tmp->next;
+    }
+    return false;//возвращает false, если значение не было найдено в списке
+}
+
+template <typename T>
+T List<T>::FindItemByNumber(int pos) {
+    if(pos>=size||pos<0) throw INVALID_INDEX;
+    else{
+        Node *tmp=head;
+        int i=0;
+        while (i!=pos){
+            tmp=tmp->next;
+            i++;
+        }
+        return *(tmp->item);
+    }
+}
+
+template <typename T>
+int List<T>::GetPos(T t) {
+    Node *tmp=head;
+    for(int i=0;i<size;i++){
+        if(*(tmp->item)==t) return i;//возвращаем позицию, на которой было найдено соответствующее значение
+        tmp=tmp->next;
+    }
+    return -1;//возвращает -1, если значение не было найдено в списке
+}
+
+template <typename T>
+bool List<T>::isEmpty() {
+    return (size>0)? false: true;
+}
+
+template <typename T>
+int List<T>::GetSize() {
+    return (!this->isEmpty())? size: throw EMPTY_LIST;
+}
+
+template <typename T>
+bool List<T>::ChangeValue(T t, int pos) {
+    if(pos>=size||pos<0) return false;
+    else{
+        Node *tmp=head;
+        int i=0;
+        while (i!=pos){
+            tmp=tmp->next;
+            i++;
+        }
+        *(tmp->item)=t;
+        return true;
+    }
+}
+
+//------------Методы класса Iterator------------
+template <typename T>
+List<T>::Iterator::Iterator(List<T> &t) {
+    list=&t;
+    cur=list->head;
+    status = false;
+}
+
+template <typename T>
+typename List<T>::Iterator& List<T>::Iterator::operator++(int) {
+    if(list->isEmpty()) throw EMPTY_LIST;
+    cur=cur->next;
+    status = true;
+    //if(!inList()) throw ITERATOR_END;
+    return *this;
+}
+
+template <typename T>
+T& List<T>::Iterator::operator*() {
+    if(inList()) return *(cur->item);
+    else throw ITERATOR_END;
+}
+
+template <typename T>
+T& List<T>::Iterator::begin() {
+    if(list->isEmpty()) throw EMPTY_LIST;
+    cur=list->head;
+    status = false;
+    return *(cur->item);
+}
+
+template <typename T>
+bool List<T>::Iterator::inList() {
+    return (!(cur==list->head&&status));//если мы уже один раз посещали голову списка и вернулись к ней, то итератор обошел весь список (возвращаем false)
+}
+
+//------------Методы класса Node---------------
+template <typename T>
+List<T>::Node::Node(){}
+
+template <typename T>
+List<T>::Node::Node(T t) {
+    item=new T(t);
+}
+
+template <typename T>
+List<T>::Node::~Node() {
+    delete item;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,207 @@
 #include <iostream>
+#include "List.h"
+
+using namespace::std;
+
+
+
+void showMenu(){
+    cout << endl;
+
+    cout << "1. Watch list" << endl;
+    cout << "2. Clear list" << endl;
+    cout << "3. Empty check" << endl;
+    cout << "4. Check for a presence of a value in list" << endl;
+    cout << "5. Search by index" << endl;
+    cout << "6. Change by index" << endl;
+    cout << "7. Find by value" << endl;
+    cout << "8. Add new value" << endl;
+    cout << "9. Add new value by index" << endl;
+    cout << "10. Delete by value" << endl;
+    cout << "11. Delete by index" << endl;
+    cout << "12. Iterator to first position" << endl;
+    cout << "13. Change value at iterator's position" << endl;
+    cout << "14. Show value at iterator's position" << endl;
+    cout << "15. Shift iterator" << endl;
+    cout << "16. List size" << endl;
+    cout << "17. Test (not realized yet)" << endl;
+    cout << "0. Exit" << endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    List<int> testList;
+    List<int>::Iterator iter(testList);
+
+    showMenu();
+
+    bool exit= false;
+    int com;
+    while (!exit){
+        cout<<"input command:";
+        cin>>com;
+        cout<< endl;
+        try{
+            switch (com){
+                case 0: exit= true;
+                    break;
+                default: cout << "No such command exist"<<endl;
+                    break;
+
+                case 1:{
+                    if(!testList.isEmpty()){
+                        iter.begin();
+                        cout<<"List contains:"<<endl;
+                        for(int i=0;i<testList.GetSize();i++, iter++){
+                            cout<<"Position: "<<i<<" Value: " << *iter<<endl;
+                        }
+                    } else{
+                        cout<<"List is empty!"<<endl;
+                    }
+                    break;
+                }
+
+                case 2:{
+                    testList.CleanList();
+                    cout<<"Cleaned"<<endl;
+                    break;
+                }
+
+                case 3:{
+                    cout<<"List status: ";
+                    (testList.isEmpty())? cout<<"empty"<<endl:cout<<"not empty"<<endl;
+                    break;
+                }
+
+                case 4:{
+                    int value;
+                    cout<<"Input value";
+                    cin>>value;
+                    cout<<endl;
+                    (testList.FindItem(value))? cout<<"List contains this value"<<endl:cout<<"List does not contain this value"<<endl;
+                    break;
+                }
+
+                case 5:{
+                    int index;
+                    cout<<"Input index";
+                    cin>>index;
+                    cout<<endl;
+                    cout<<"Position value: "<<testList.FindItemByNumber(index)<<endl;
+                    break;
+                }
+
+                case 6:{
+                    int ind;
+                    cout<<"Input index";
+                    cin>>ind;
+                    cout<<endl;
+                    int val;
+                    cout<<"Input value";
+                    cin>>val;
+                    cout<<endl;
+                    (testList.ChangeValue(val,ind))? cout<<"Value successfully changed"<<endl:cout<<"Can't change value"<<endl;
+                    break;
+                }
+
+                case 7:{
+                    int vl;
+                    cout<<"Input value";
+                    cin>>vl;
+                    cout<<endl;
+                    int posIt=testList.GetPos(vl);
+                    (posIt==-1)? cout<<"List does not contain such value"<<endl:cout<<"Value position: "<<posIt<<endl;
+                    break;
+                }
+
+                case 8:{
+                    int val1;
+                    cout<<"Input value:";
+                    cin>>val1;
+                    cout<<endl;
+                    testList.AddItem(val1);
+                    cout<<"Item added"<<endl;
+                    break;
+                }
+
+                case 9:{
+                    int indx;
+                    cout<<"Input index";
+                    cin>>indx;
+                    cout<<endl;
+                    int val2;
+                    cout<<"Input value";
+                    cin>>val2;
+                    cout<<endl;
+                    (testList.AddItemByNumber(val2,indx))? cout<<"Successfully added"<<endl:cout<<"Cannot add value in this position"<<endl;
+                    break;
+                }
+
+                case 10:{
+                    int val3;
+                    cout<<"Input value";
+                    cin>>val3;
+                    cout<<endl;
+                    (testList.DeleteByValue(val3))? cout<<"Successfully deleted"<<endl:cout<<"Cannot delete"<<endl;
+                    break;
+                }
+
+                case 11:{
+                    int indx1;
+                    cout<<"Input index";
+                    cin>>indx1;
+                    cout<<endl;
+                    (testList.DeleteByNumber(indx1))? cout<<"Successfully deleted"<<endl:cout<<"Cannot delete"<<endl;
+                    break;
+                }
+
+                case 12:{
+                    iter.begin();
+                    cout<<"Iterator successfully moved at beginning of list"<<endl;
+                    break;
+                }
+
+                case 13:{
+                    int newVal;
+                    cout<<"Input value";
+                    cin>>newVal;
+                    cout<<endl;
+                    *iter=newVal;
+                    break;
+                }
+
+                case 14:{
+                    cout<<"Current value at iterator's position: "<<*iter<<endl;
+                    break;
+                }
+
+                case 15:{
+                    iter++;
+                    cout<<"Iterator moved to next element"<<endl;
+                    break;
+                }
+
+                case 16:{
+                    cout<<"List size: "<<testList.GetSize()<<endl;
+                    break;
+                }
+
+                case 17:{
+                    break;
+                }
+            }
+        }catch (int code){
+            cout<<"Error: ";
+            switch (code){
+                case INVALID_INDEX: cout<<"invalid index"<<endl;
+                    break;
+                case EMPTY_LIST: cout<<"List is empty"<<endl;
+                    break;
+                case ITERATOR_END: cout<<"Iterator is out of list"<<endl;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     return 0;
 }
